391351bd01b4c4785333cda6477d22f5b571d565
supertiger <supertiger@gmail.com>
2025-07-03 22:01:01 +0900

    Remove 29 game resources M ~ Z

--- PrinceNeZhaJW/src/settings.744b6.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/LoadingScene.fire",orientation:"portrait",jsList:[],bundleVers:{resources:"a9fce",internal:"d17e2",main:"b612b"}};
